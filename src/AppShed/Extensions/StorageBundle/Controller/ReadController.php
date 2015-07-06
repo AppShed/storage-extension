72d0e3a25d0113367a789424e38faf23275a1f0b
Igor Dubiy <igor-dubiy@ekreative.com>
2015-07-06 19:04:03 +0300

    fix read data on unexpected array

--- src/AppShed/Extensions/StorageBundle/Controller/ReadController.php
+++ src/AppShed/Extensions/StorageBundle/Controller/ReadController.php
@@ -98,10 +98,16 @@ class ReadController extends StorageController
                     unset($data['title']);
                 } else {
                     $title = current($data);
+                    if (is_array($title)) {
+                        $title = array_keys($data)[0];
+                    }
                 }
 
                 $dataScreen = new Screen($title);
                 foreach ($data as $key => $value) {
+                    if (is_array($value)) {
+                        $value = '[' . implode(', ', $value) . ']';
+                    }
                     $dataScreen->addChild(new Thumb($key, $value));
                 }
 
